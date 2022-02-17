9f782bf4eef743aade841037ec0f2c285f235d94
Costas Likos <costaslikos@gmail.com>
2022-02-17 13:34:54 +0200

    First initial push

---
+++ Controllers/TrainerController.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Assignment2.Models;
+using Assignment2.MyDataBase;
+using PagedList;
+using PagedList.Mvc;
+
+
+namespace Assignment2.Controllers
+{
+    public class TrainerController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Trainer
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = sortOrder == "Name" ? "name_desc" : "Name";
+            ViewBag.SalarySortParm = sortOrder == "Salary" ? "salary_desc" : "Salary";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+
+            var trainers = from tr in db.Trainers
+                           select tr;
+            
+            //search bar ******==============
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                trainers = trainers.Where(s => s.Name.Contains(searchString));
+            }
+
+            //search bar *****==============
+
+            switch (sortOrder)
+            {
+                case "Name":
+                    trainers = trainers.OrderBy(tr => tr.Name);
+                    break;
+                case "Salary":
+                    trainers = trainers.OrderBy(tr => tr.Salary);
+                    break;
+                default:
+                    trainers = trainers.OrderBy(tr => tr.TrainerId);
+                    break;
+            }
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(trainers.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Trainer/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // GET: Trainer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Trainer/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TrainerId,Name,Salary")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Trainers.Add(trainer);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(trainer);
+        }
+
+        // GET: Trainer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // POST: Trainer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TrainerId,Name,Salary")] Trainer trainer)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(trainer).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(trainer);
+        }
+
+        // GET: Trainer/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Trainer trainer = db.Trainers.Find(id);
+            if (trainer == null)
+            {
+                return HttpNotFound();
+            }
+            return View(trainer);
+        }
+
+        // POST: Trainer/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Trainer trainer = db.Trainers.Find(id);
+            db.Trainers.Remove(trainer);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MyDataBase/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assignment2.MyDataBase
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("LinkDb")
+        {
+
+        }
+
+        public DbSet<Trainer> Trainers { get; set; }
+    }
+   
+}
